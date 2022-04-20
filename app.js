8e6f0603c1ca69056e200a806a933aac6ab3058f
esben holk nielsen <esbenholknielsen@gmail.com>
2022-04-20 13:41:20 +0200

    onboarding function

--- app.js
+++ app.js
@@ -41,11 +41,7 @@ app.post("/webhook", function (req, res) {
     req.body.entry.forEach(function(entry) {
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        } else if (event.message) {
-          processMessage(event);
-        }
+        check_if_player_is_new(event);
       });
     });
 
@@ -54,8 +50,7 @@ app.post("/webhook", function (req, res) {
 });
 
 
-function processMessage(event) {
-
+function check_if_player_is_new(event) {
 
   if (!event.message.is_echo) {
     console.log("receives message:  ", event.message);
@@ -68,71 +63,94 @@ function processMessage(event) {
       .then(result => {
          
           console.log("GETS USER", result);
-          
           if(result.rowCount === 0){
+
             databaseActions.createUser(senderId, username)
             .then(result => {
-                console.log("CREATED USER", result);
+              console.log("CREATED USER", result);
+                beginOnBoarding(event);
               }).catch(err => {
-               console.log(err);
+              console.log(err);
             });
+
+          } else{
+            
+            if (event.postback) {
+              processPostback(event);
+            } else if (event.message) {
+              processMessage(event);
+            }
+            
           }
         }).catch(err => {
          console.log("DOES NOT GET USER", err);
       });
 
-
-    console.log("Received message from senderId: " + senderId);
-    console.log("Message is: " + JSON.stringify(message));
-
-    // You may get a text or attachment but not both
-    if (message.text) {
-      var formattedMsg = message.text.toLowerCase().trim();
-      if(formattedMsg === "hej"){
-
-        response = {
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": [{
-                "title": "I found this image of you",
-                "subtitle": "Do you recognise yourself?",
-                "image_url": "https://res.cloudinary.com/www-houseofkilling-com/image/upload/v1649845465/Britta%20Spyd/image_efzzgz.png",
-                "buttons": [
-                  {
-                    "type": "postback",
-                    "title": "Yes!",
-                    "payload": "yes",
-                  },
-                  {
-                    "type": "postback",
-                    "title": "No!",
-                    "payload": "no",
-                  }
-                ],
-              }]
-            }
-          }
-        }
-        sendMessage(senderId, response);
-      }
-
-
     } else if (message.attachments) {
       let attachment_url = message.attachments[0].payload.url;
 
-      sendMessage(senderId, "Sorry, I don't understand your request.");
+      sendMessage(senderId, "Sorry, I don't understand that. I am not so good at recognising images yet");
     }
+  
+}
+
+function beginOnBoarding(event){
+
+  if (!event.message.is_echo) {
+    console.log("receives message:  ", event.message);
+    var message = event.message;
+    var senderId = event.sender.id;
+
+    console.log("ONBOARDING " + senderId);
+    if (message.text) {
+
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Welcome to the Britta Spyd Experience!",
+              "subtitle": "My name is Aase and I will be your guide through this site specific puzzle game, where you have to discover the building we are in and the items that are left here. Did you know that this building was built in 1894 to house the Public Trustee, a national institution that governed the estate of people deemed unable to govern themselves. Back then that mostly meant orphans and children. You will play as Britta Spyd, a young woman recently widowed who has just appeared on the buildings doorstep in the hopes that she can get access to her money. Do you wanna play?",
+              "image_url": "https://res.cloudinary.com/www-houseofkilling-com/image/upload/v1650453963/Britta%20Spyd/Aase_addvdh.jpg",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+      sendMessage(senderId, response);
+    
+  } 
+
   }
+
 }
 
 function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
+  console.log("IS ALREADY PLAYER, and sends payload", payload);
 
 
+}
 
+function processMessage(event) {
+  var senderId = event.sender.id;
+  var message = event.message;
+  var payload = event.postback.payload;
+
+  console.log("IS ALREADY PLAYER, and sends message", message);
 
 }
 
