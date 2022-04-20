a452311fce196b84d2c77b74bf435a1437155d29
esben holk nielsen <esbenholknielsen@gmail.com>
2022-04-20 16:39:08 +0200

    trim lowercase string message

--- app.js
+++ app.js
@@ -242,9 +242,10 @@ function processPostback(event) {
 
 function processMessage(event) {
   var senderId = event.sender.id;
-  var message = event.message.text.toLowerCase();
+  var message = event.message;
+  var formattedMessage = message.text.toLowerCase().trim();
 
-  if(message.contains("agnes")){
+  if(formattedMessage.contains("agnes")){
     
       console.log("IS ALREADY PLAYER, and sends message", message);
       response = {
@@ -252,7 +253,7 @@ function processMessage(event) {
       }
       sendMessage(senderId, response);
 
-  } else if(message.contains("reception")){
+  } else if(formattedMessage.contains("reception")){
     
     response = {
       "text": "Welcome to the Public Trustee. As the Danish Government doesn't allow lone women to own any wealth, we offer to help widows like yourself to regain ownership over their belongings. All you have to do is pick up a form and bring it to the Director's office. If you're not ready, you can come back later with more information by simply typing reception. Enjoy your visit!"
@@ -267,13 +268,13 @@ function processMessage(event) {
       sendMessage(senderId, response);
     }, 3000);
 
-  } else if(message.contains("director")){
+  } else if(formattedMessage.contains("director")){
     response = {
       "text": "you are standing in the Directors office, are you ready to see him?"
     }
     sendMessage(senderId, response);
 
-  } else if(message.contains("atelier")){
+  } else if(formattedMessage.contains("atelier")){
     response = {
       "text": "you are standing in the atelier. This is a place to wait and maybe meet a stranger. Oh! look at that: Ole is here. Lets chat with him"
     }
